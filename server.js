a6807752edccd3887cbdd0c1622c29966f7ce9f9
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-07-23 18:22:24 -0500

    completed server.js and config folder

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connect = require("./config/connection");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+connect.once(" open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
