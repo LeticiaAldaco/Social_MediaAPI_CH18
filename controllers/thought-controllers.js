a7b534b1a93603fb1b73d5d87ed05538e4e72495
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-07-23 18:39:28 -0500

    created get all thoughts, get thought by id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -1,1 +1,23 @@
 const { Thought, User } = require("../models");
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find()
+      // sorting from newest to oldest
+      .sort({ createdAt: -1 })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = thoughtController;
