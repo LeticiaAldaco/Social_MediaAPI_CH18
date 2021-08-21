b2280177d1703438cb4743bf4ac25dfd8e6fa6ea
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-20 21:41:22 -0500

    trouble shooting

--- models/User.js
+++ models/User.js
@@ -17,6 +17,12 @@ const UserSchema = new Schema(
     thought: [
       {
         type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
         ref: "User",
       },
     ],
@@ -33,6 +39,6 @@ UserSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-const User = model("user", UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
